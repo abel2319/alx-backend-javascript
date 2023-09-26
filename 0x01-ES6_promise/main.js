90d27130690c364fa8f157c800d9027f44399d09
abel2319 <abelbakary@gmail.com>
2023-09-26 22:12:02 +0100

    task 00

--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -2,4 +2,4 @@ import getResponseFromAPI from "./0-promise.js";
 
 //const response = getResponseFromAPI();
 console.log(getResponseFromAPI())
-//console.log(response instanceof Promise);
+console.log(response instanceof Promise);
