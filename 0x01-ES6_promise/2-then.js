ae12575880bc3fb1a970f3c9dd342f0ccaebd980
abel2319 <abelbakary@gmail.com>
2023-09-28 10:39:03 +0100

    task 2 refactor

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
-    status: 200,
-    body: 'Success',
-  })).catch(() => new Error()).finally(() => {
-    console.log('Got a response from the API');
-  });
+  promise
+    .then(() => ({
+      status: 200,
+      body: 'Success',
+    }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
