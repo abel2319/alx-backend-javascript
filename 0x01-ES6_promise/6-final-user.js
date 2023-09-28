bb3420ab27b47d616ccae6b73397cad965a3ade1
abel2319 <abelbakary@gmail.com>
2023-09-28 14:57:09 +0100

    task 6 fix return

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,12 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-/* eslint-disable no-unused-vars */
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(), uploadPhoto()])
-    .then(() => [
-      {
-        status: 'status_of_the_promise',
-        value: 'value or error returned by the Promise',
-      },
-    ])
-    .catch(() => {});
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+        res.map((obj) => ({
+          status: obj.status,
+          value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
+        }))
+    ));
 }
