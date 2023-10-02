0cce3005a9a308f6745c006826ef88cc05131bcf
abel2319 <abelbakary@gmail.com>
2023-10-02 20:31:28 +0100

    feat(task): task 7 ✅

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,6 @@
+export default function hasValuesFromArray(set, array) {
+  if (!(set instanceof Set)) { throw new TypeError('set must be a Set'); }
+  if (!(array instanceof Array)) { throw new Error('array must be a Array'); }
+
+  return array.every((elt) =>  set.has(elt) );
+}
