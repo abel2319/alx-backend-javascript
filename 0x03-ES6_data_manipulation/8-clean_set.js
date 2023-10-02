e9fcee85af2997f1567b83f8febdbf24792b3545
abel2319 <abelbakary@gmail.com>
2023-10-02 23:01:00 +0100

    feat(task9): task 9 ✅

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, startString) {
   if (!(set instanceof Set)) { throw new Error('set must be a Set'); }
   if (typeof startString !== 'string') { throw new Error('startString must be a String'); }
 
-  if (startString !== '') {
+  if (typeof startString === 'string' && startString !== '') {
     for (const elt of set) {
       if (elt.startsWith(startString)) {
         string.push(elt.substring(startString.length));
