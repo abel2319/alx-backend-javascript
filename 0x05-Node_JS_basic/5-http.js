9583fe3e3b4b046e0d9d43358833c46d9dea20bd
abel2319 <abelbakary@gmail.com>
2023-11-24 17:53:15 +0100

    fix(task5): task5 🩹 eslint

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,23 +4,6 @@ const students = require('./3-read_file_async');
 const host = 'localhost';
 const port = 1245;
 
-const requestListener = (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    students(process.argv[2]).then((data) => {
-      res.write(`Number of students: ${data.stud.length}\n`);
-      res.write(`Number of students in CS: ${data.csStud.length}. List: ${data.csStud.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${data.sweStud.length}. List: ${data.sweStud.join(', ')}`);
-      res.end();
-    }).catch((err) => res.end(err.message));
-  }
-};
-
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -29,7 +12,7 @@ const app = http.createServer((req, res) => {
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
     students(process.argv[2]).then((data) => {
-      res.write(`Number of students: ${data.arr.length}\n`);
+      res.write(`Number of students: ${data.arr.length - 2}\n`);
       res.write(`Number of students in CS: ${data.csStud.length}. List: ${data.csStud.join(', ')}\n`);
       res.write(`Number of students in SWE: ${data.sweStud.length}. List: ${data.sweStud.join(', ')}`);
       res.end();
