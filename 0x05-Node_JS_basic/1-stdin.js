2c6b7b9a2d37ff6e61fb7d8f71bbcbdd33772428
abel2319 <abelbakary@gmail.com>
2023-11-24 08:14:46 +0100

    feat(task1):task 1 ✅

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', () => {
+  const line = process.stdin.read();
+
+  if (line) {
+    process.stdout.write(`Your nae is: ${line}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
